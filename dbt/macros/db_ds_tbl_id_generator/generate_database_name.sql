dade5258f0e25a06a5787d80af707a02d654144a
charles verleyen <charles@addastra.io>
2023-01-31 12:25:23 +0100

    update for newcomers

--- dbt/macros/db_ds_tbl_id_generator/generate_database_name.sql
+++ dbt/macros/db_ds_tbl_id_generator/generate_database_name.sql
@@ -4,15 +4,15 @@
     {% set is_prd =         (target.name in ['prd']) %}
     {% set is_seeds =       (node.resource_type == 'seed') %}
     {% set is_test =        (node.resource_type == 'test') %}
-    {% set is_re_data =     ('re_data' in node.fqn)  %}
+    {% set is_project_evaluator = ('dbt_project_evaluator' in node.fqn) %}
+    {% set is_elementary =     ('elementary' in node.fqn[0])  %}
+    {% set is_dbt_artifact = (('dbt_artifacts' in node.package_name) or ('dbt_artifacts' in node.raw_sql)) | as_bool %}
     {% set is_metrics =     ('metrics' in node.fqn[0])  %}
 
     {% set is_layer_staging = ('staging' in node.fqn) %}
     {% set is_layer_intermediate = ('intermediate' in node.fqn) %}
     {% set is_layer_data_warehouse = ('data_warehouse' in node.fqn) %}
     {% set is_layer_datamart = ('datamart' in node.fqn) %}
-    {% set is_dbt_artifact = (('dbt_artifacts' in node.package_name) or ('dbt_artifacts' in node.raw_sql)) | as_bool %}
-
 
     {%- set error_unresolve_message -%}
         {{ node.resource_type | capitalize }} '{{ node.unique_id }}' unable to resolve database name.
@@ -32,9 +32,12 @@
       {%- elif is_layer_datamart -%}        {{- 'dbt-demo-aaa' -}}
       {%- elif is_seeds -%}                 {{- 'dbt-demo-aaa' -}}
       {%- elif is_test -%}                  {{- 'dbt-demo-aaa' -}}
+
+      {%- elif is_project_evaluator -%}     {{- 'dbt-demo-aaa' -}}
       {%- elif is_dbt_artifact -%}          {{- 'dbt-demo-aaa' -}}
-      {%- elif is_re_data -%}               {{- 'dbt-demo-aaa' -}}
+      {%- elif is_elementary -%}            {{- 'dbt-demo-aaa' -}}
       {%- elif is_metrics -%}               {{- 'dbt-demo-aaa' -}}
+
       {%- else -%}
       {{ log("TOTO: " ~ node) }}
       {{ exceptions.raise_compiler_error(error_unresolve_message) }}
@@ -48,11 +51,14 @@
       {%- elif is_layer_datamart -%}        {{- 'dbt-demo-aaa' -}}
       {%- elif is_seeds -%}                 {{- 'dbt-demo-aaa' -}}
       {%- elif is_test -%}                  {{- 'dbt-demo-aaa' -}}
+
+      {%- elif is_project_evaluator -%}     {{- 'dbt-demo-aaa' -}}
       {%- elif is_dbt_artifact -%}          {{- 'dbt-demo-aaa' -}}
-      {%- elif is_re_data -%}               {{- 'dbt-demo-aaa' -}}
+      {%- elif is_elementary -%}            {{- 'dbt-demo-aaa' -}}
       {%- elif is_metrics -%}               {{- 'dbt-demo-aaa' -}}
-      {%- else -%}                          {{ exceptions.raise_compiler_error(error_unresolve_message) }}
-      {%- endif -%}
+    {%- else -%}
+        {{ exceptions.raise_compiler_error(error_unresolve_message) }}
+    {%- endif -%}
 
     {%- endif -%}
 {%- endmacro %}
--- dbt/macros/db_ds_tbl_id_generator/generate_schema_name.sql
+++ dbt/macros/db_ds_tbl_id_generator/generate_schema_name.sql
@@ -1,21 +1,22 @@
 {% macro generate_schema_name(custom_schema_name=none, node=none) -%}
 
+    {% set is_elementary =     ('elementary' in node.fqn[0])  %}
+    {% set is_project_evaluator = ('dbt_project_evaluator' in node.fqn) %}
     {% set is_dbt_artifact = ('dbt_artifacts' in node.package_name) | as_bool %}
+    {% set is_sbx =         ('sbx' in target.name ) %}
+    {% set is_prd =         ('prd' in target.name ) %}
 
-    {# handling the udf #}
-    {%- if custom_schema_name =='bqdts_udf' -%}
-        {{ custom_schema_name -}}
+    {% if is_sbx %}
+        {% set suffix =     "sbx" %}
+    {% elif is_prd %}
+        {% set suffix =     "prd" %}
+    {%- else -%}
+      {{ exceptions.raise_compiler_error(error_unresolve_message) }}
+    {%- endif -%}
 
-    {# handling snapshot #}
-    {%- elif custom_schema_name=='snapshot'  -%}
-        {{- 'bqdts_' ~ node.fqn[1] -}}
-        {{ default_schema }}
-
-    {%- elif custom_schema_name is not none -%}
+    {%- if custom_schema_name is not none -%}
         {# handling test #}
-        {%- if node.resource_type == 'test' -%}  {{- 'bqdts_' ~ node.fqn[1] ~ '_tests_' ~ target.name -}}
-        {%- elif 're_data' in node.fqn  %}
-          {{- 'bqdts_' ~ target.name ~ '_re_data' -}}
+        {%- if node.resource_type == 'test' -%}  {{- 'bqdts_' ~ node.tags[0] ~ '_tests_' ~ target.name -}}
         {%- else -%}
             {%- set error_message -%}
             {{ node.resource_type | capitalize }} '{{ node.unique_id }}' has a schema configured. This is not allowed.
@@ -29,12 +30,20 @@
     {%- elif node.resource_type == 'seed' -%}
         {{- 'bqdts_' ~ target.name ~ '_mapping' -}}
 
+    {# handling elementary #}
+    {%- elif custom_schema_name is none and is_elementary -%}
+        {{- 'bqdts_' ~ suffix ~ node.tags[0] ~ '_elementary' -}}
+
+    {# handling dbt project evaluator #}
+    {%- elif custom_schema_name is none and is_project_evaluator -%}
+        {{- 'bqdts_' ~ suffix ~ node.tags[0] ~ '_project_evaluator' -}}
+
     {# handling dbt_artifacts package #}
     {%- elif custom_schema_name is none and is_dbt_artifact -%}
         {{- 'bqdts_' ~ target.name ~ '_artifacts' -}}
 
     {%- elif custom_schema_name is none -%}
-        {{- 'bqdts_' ~ target.name ~ '_' ~ node.fqn[1] -}}
+        {{- 'bqdts_' ~ target.name ~ '_' ~ node.tags[0]  -}}
     {%- endif -%}
 
 {%- endmacro %}
