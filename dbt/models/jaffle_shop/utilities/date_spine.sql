585c7f0e257d241e2620a051534813e2048a94e5
charles verleyen <charles@astrafy.io>
2023-03-08 21:36:14 +0100

    feat(readme): update readme

--- dbt/models/jaffle_shop/utilities/date_spine.sql
+++ dbt/models/jaffle_shop/utilities/date_spine.sql
@@ -10,6 +10,6 @@
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2020-01-01' as date)",
-    end_date="cast('2020-12-11' as date)"
+    end_date="cast('2020-12-31' as date)"
    )
 }}
